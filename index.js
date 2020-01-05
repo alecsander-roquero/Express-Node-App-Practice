75e425f1eb4ea60884d9e2433f56d3baa6908695
a021 <alecs_roquero@yahoo.com>
2020-01-05 20:10:54 +0800

    Try 16

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req,res) => {
-    res.send({ hello: 'world' });
+    res.send({ gg: 'Good Game!!!!' });
 });
 
 const PORT = process.env.PORT || 5000; 

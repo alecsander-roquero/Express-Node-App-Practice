016b46cd8ba8b0626ca2c8db071e5819f1cc17f3
a021 <alecs_roquero@yahoo.com>
2020-01-08 11:39:54 +0800

    Updated 25

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ passport.use(
     })
 );
 
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
 app.get('/', (req,res) => {
     res.send({ gg: 'Good Game!!!!' });
 });

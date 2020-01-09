25ed94c507b5a4e764f5efdf4860db6d77b79ef1
a021 <alecs_roquero@yahoo.com>
2020-01-09 13:33:06 +0800

    Updated 37

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: '17042631237-s24sdgnumv6d662ect3g4ir3itffvcml.apps.googleusercontent.com',
     googleClientSecret: '7gtlimwgC-tFeBNT5rB_0BEV',
+    mongoURI: 'mongodb+srv://username-dev:password-dev@mongopractice-hkrcs.mongodb.net/test?retryWrites=true&w=majority',
 };

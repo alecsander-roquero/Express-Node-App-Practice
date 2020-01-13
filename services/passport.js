afc13e8e85104983d931053d76af0eaed3f54f42
a021 <alecs_roquero@yahoo.com>
2020-01-13 18:57:49 +0800

    Updated 44

--- services/passport.js
+++ services/passport.js
@@ -5,10 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users', mongoose.userSchema);
 
+//serializes user
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null,user);
+    });
+})
 passport.use(
     new GoogleStrategy({
     clientID: keys.googleClientID,

744ac28ed58a56c354c6378ae88000b4d13cda98
a021 <alecs_roquero@yahoo.com>
2020-02-06 20:34:02 +0800

    Updated 76

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,8 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" component={Landing}/>
+                    <Route exact path="/" component={Landing}/>
+                    <Route path="/surveys" component={SurveyNew}/>
                 </div>
             </BrowserRouter>
         </div>

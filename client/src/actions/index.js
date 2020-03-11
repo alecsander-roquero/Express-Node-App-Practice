b375d4df19481b05e30a8231e1a1a85ec80d809c
a021 <alecs_roquero@yahoo.com>
2020-03-11 17:50:37 +0800

    Updated 86

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import {FETCH_USER} from './types';
 
-export const fetchUser = () => {
-   return function(dispatch){
-        axios.get('api/current_user')
-        .then(res => dispatch({ type: FETCH_USER, payload: res, res }));
-   }
+export const fetchUser = () => async (dispatch) => {
+      const res = await axios.get('api/current_user')
+      dispatch({ type: FETCH_USER, payload: res });
 }
+
+/* 
+another way to refractor
+   export const fetchUser = () => async (dispatch) => {
+   dispatch({ type: FETCH_USER, payload: await axios.get('api/current_user') });
+} */
